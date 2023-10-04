9b042cf41255bcf1d010a427ef0aa87293259104
Alerson Jorge <ajotanc@gmail.com>
2023-10-04 10:56:05 -0300

    new test

---
+++ api/serverless.ts
@@ -0,0 +1,22 @@
+import * as dotenv from "dotenv";
+import { appRoutes } from "@/http/routes";
+
+// Require the framework
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+
+dotenv.config();
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: false,
+});
+
+// Register your application as a normal plugin.
+app.register(appRoutes, {
+  prefix: "/",
+});
+
+export default async (request: FastifyRequest, reply: FastifyReply) => {
+  await app.ready();
+  app.server.emit("request", request, reply);
+};
