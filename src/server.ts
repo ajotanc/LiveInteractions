32dbf4cd77b3ef8e152f00ae4ccbad3eef0b8768
Alerson Jorge <ajotanc@gmail.com>
2024-08-06 03:01:21 -0300

    atualização

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { env } from "./env";
 app
   .listen({
     host: "0.0.0.0",
-    port: env.PORT,
+    port: process.env.PORT ? Number(process.env.PORT) : env.PORT,
   })
   .then(() => {
     console.log("HTTP Server Running!");
