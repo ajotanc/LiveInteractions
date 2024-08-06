32d260a784eac12898e1ced19712d91a5d35d2a8
Alerson Jorge <ajotanc@gmail.com>
2024-08-06 02:30:29 -0300

    update puppeteer

--- src/http/controllers/games/steam.ts
+++ src/http/controllers/games/steam.ts
@@ -1,5 +1,5 @@
-import puppeteer from "puppeteer";
-import chromium  from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
+import chrome from "chrome-aws-lambda";
 import cheerio from "cheerio";
 import { createClient } from "@supabase/supabase-js";
 
@@ -15,159 +15,145 @@ process.setMaxListeners(0);
 const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
 
 interface Summary {
-  description: string;
-  review: string;
-  top: number;
-  tags: string[];
+	description: string;
+	review: string;
+	top: number;
+	tags: string[];
 }
 
 interface Games {
-  name: string;
-  value: number;
-  url: string;
-  summary: Summary;
+	name: string;
+	value: number;
+	url: string;
+	summary: Summary;
 }
 
-let browser = null;
+export default async function getContent(url: string) {
+	const options = process.env.AWS_REGION
+		? {
+				args: chrome.args,
+				executablePath: await chrome.executablePath,
+				headless: chrome.headless,
+			}
+		: {
+				args: [],
+				executablePath:
+					process.platform === "win32"
+						? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+						: process.platform === "linux"
+							? "/usr/bin/google-chrome"
+							: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+			};
+
+	const browser = await puppeteer.launch(options);
+	const page = await browser.newPage();
+
+	await page.goto(url, { waitUntil: "networkidle2", timeout: 0 });
+
+	const content = await page.content();
+	await browser.close();
+
+	return content;
+}
 
 export async function mostPlayed(request: FastifyRequest): Promise<Games[]> {
-  const choices = ["10", "25", "50", "75", "100"] as [string, ...string[]];
+	const choices = ["10", "25", "50", "75", "100"] as [string, ...string[]];
 
-  const mostPlayedQuerySchema = z.object({
-    top: z.enum(choices).default("10"),
-  });
+	const mostPlayedQuerySchema = z.object({
+		top: z.enum(choices).default("10"),
+	});
 
-  const { top } = mostPlayedQuerySchema.parse(request.query);
+	const { top } = mostPlayedQuerySchema.parse(request.query);
 
-  const today = new Date();
-  const dateStr = today.toISOString().split("T")[0];
-  const filename = `${dateStr}-${top}.json`;
+	const today = new Date();
+	const dateStr = today.toISOString().split("T")[0];
+	const filename = `${dateStr}-${top}.json`;
 
-  const { data: fileExists } = await supabase.storage
-    .from("most-played")
-    .download(filename);
+	const { data: fileExists } = await supabase.storage
+		.from("most-played")
+		.download(filename);
 
-  if (fileExists) {
-    const fileContent = await fileExists.text();
-    return JSON.parse(fileContent);
-  }
+	if (fileExists) {
+		const fileContent = await fileExists.text();
+		return JSON.parse(fileContent);
+	}
 
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    browser = await chromium.puppeteer.launch({
-      args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-         defaultViewport: chromium.defaultViewport,
-         executablePath: await chromium.executablePath,
-         headless: true,
-         ignoreHTTPSErrors: true,
-     });
-  } else {
-    browser = await puppeteer.launch();
-  }
+	const content = await getContent(
+		"https://store.steampowered.com/charts/mostplayed/",
+	);
+	const $ = cheerio.load(content);
 
-  const page = await browser.newPage();
+	const games = [];
 
-  await page.goto("https://store.steampowered.com/charts/mostplayed/", {
-    waitUntil: "networkidle2",
-    timeout: 0,
-  });
+	const rows = $('[data-featuretarget="react-root"]')
+		.find("table tbody")
+		.find("tr")
+		.filter((index) => index <= Number.parseInt(top));
 
-  const content = await page.content();
-  const $ = cheerio.load(content);
+	for (const element of rows.toArray()) {
+		const label = $(element).find("td:eq(2) a > div").text().trim();
+		const url = $(element).find("td:eq(2) a").attr("href");
+		const value = Number.parseInt(
+			$(element).find("td:eq(5)").text().trim().replace(/\D/g, ""),
+		);
 
-  const games = [];
+		delay(10);
 
-  const rows = $('[data-featuretarget="react-root"]')
-    .find("table tbody")
-    .find("tr")
-    .filter((index) => index <= Number.parseInt(top));
+		const summary = await getInfo(url);
+		games.push({
+			label,
+			value,
+			url,
+			summary,
+		});
+	}
 
-  for (const element of rows.toArray()) {
-    const label = $(element).find("td:eq(2) a > div").text().trim();
-    const url = $(element).find("td:eq(2) a").attr("href");
-    const value = Number.parseInt(
-      $(element).find("td:eq(5)").text().trim().replace(/\D/g, ""),
-    );
+	const jsonData = JSON.stringify(games, null, 2);
+	const buffer = Buffer.from(jsonData);
 
-    delay(10);
+	const { error: uploadError } = await supabase.storage
+		.from("most-played")
+		.upload(filename, buffer, {
+			contentType: "application/json",
+		});
 
-    const summary = await getInfo(url);
-    games.push({
-      label,
-      value,
-      url,
-      summary,
-    });
-  }
+	if (uploadError) {
+		throw uploadError;
+	}
 
-  await browser.close();
-
-  const jsonData = JSON.stringify(games, null, 2);
-  const buffer = Buffer.from(jsonData);
-
-  const { error: uploadError } = await supabase.storage
-    .from("most-played")
-    .upload(filename, buffer, {
-      contentType: "application/json",
-    });
-
-  if (uploadError) {
-    throw uploadError;
-  }
-
-  return games;
+	return games;
 }
 
 export async function getInfo(url: string): Promise<Summary> {
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    browser = await chromium.puppeteer.launch({
-      args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-         defaultViewport: chromium.defaultViewport,
-         executablePath: await chromium.executablePath,
-         headless: true,
-         ignoreHTTPSErrors: true,
-     });
-  } else {
-    browser = await puppeteer.launch();
-  }
+	const content = await getContent(url);
+	const $ = cheerio.load(content);
 
-  const page = await browser.newPage();
+	const infos = $("#game_highlights").find(".glance_ctn");
+	const description = infos.find(".game_description_snippet").text().trim();
 
-  await page.goto(url, {
-    waitUntil: "networkidle2",
-    timeout: 0,
-  });
+	const summary = $("#userReviews").find(".user_reviews_summary_row").last();
 
-  const content = await page.content();
-  const $ = cheerio.load(content);
+	const review = summary.find(".game_review_summary").text().trim();
+	const top = Number.parseInt(
+		summary.find(".responsive_hidden").text().replace(/\D/g, ""),
+	);
 
-  const infos = $("#game_highlights").find(".glance_ctn");
-  const description = infos.find(".game_description_snippet").text().trim();
+	const allTags = infos.find("#glanceCtnResponsiveRight").find(".app_tag");
 
-  const summary = $("#userReviews").find(".user_reviews_summary_row").last();
+	const tags = [];
 
-  const review = summary.find(".game_review_summary").text().trim();
-  const top = Number.parseInt(
-    summary.find(".responsive_hidden").text().replace(/\D/g, ""),
-  );
+	allTags.each((_, element) => {
+		const value = $(element).text().trim();
 
-  const allTags = infos.find("#glanceCtnResponsiveRight").find(".app_tag");
+		if ($(element).css("display") !== "none" && value !== "+") {
+			tags.push(value);
+		}
+	});
 
-  const tags = [];
-
-  allTags.each((_, element) => {
-    const value = $(element).text().trim();
-
-    if ($(element).css("display") !== "none" && value !== "+") {
-      tags.push(value);
-    }
-  });
-
-  await browser.close();
-
-  return {
-    description,
-    review,
-    top,
-    tags,
-  };
+	return {
+		description,
+		review,
+		top,
+		tags,
+	};
 }
